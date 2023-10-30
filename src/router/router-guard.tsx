e7d22b45f8d17eaf788feb2bfd8fbf0ced4b940a
ZhangSheng <yanglee2421@gmail.com>
2023-10-30 10:34:03 +0800

    sort out code

--- src/router/router-guard.tsx
+++ src/router/router-guard.tsx
@@ -1,9 +1,6 @@
 // MUI Imports
 import { Backdrop, CircularProgress } from "@mui/material";
 
-// Hooks Imports
-import { useAuthRoute } from "./use-auth-route";
-
 // Redux Imports
 import {
   mutateSettings,
@@ -13,22 +10,67 @@ import {
 } from "@/redux";
 
 // React Imports
-import { useEffect } from "react";
+import React from "react";
+
+// Router Imports
+import {
+  useMatches,
+  useSearchParams,
+  Navigate,
+  useOutlet,
+} from "react-router-dom";
+import { toIsInWl } from "./router-whitelist";
 
 export function Component() {
   const dispatch = useAppDispatch();
   const isLoading = useAppSelector((s) => s.login.isLoading);
+  const usr = useAppSelector((s) => s.login.usr);
 
-  useEffect(() => {
+  React.useEffect(() => {
     dispatch(mutateSettings({}));
     dispatch(mutateLogin({}));
   }, [dispatch]);
 
-  const routeEl = useAuthRoute();
+  // Router Hooks
+  const outlet = useOutlet();
+  const matches = useMatches();
+  const [searchParams] = useSearchParams();
+
+  const routeNode = React.useMemo(() => {
+    const nextRoute = matches[matches.length - 1];
+    if (!nextRoute) {
+      console.error("Invalid route");
+      return null;
+    }
+
+    // To Login
+    const isToLogin = nextRoute.id === "login";
+    if (isToLogin) {
+      const returnURL = searchParams.get("returnURL") || "/";
+      return usr ? <Navigate to={returnURL} replace /> : outlet;
+    }
+
+    // To Whitelist
+    const isInWhitelist = toIsInWl(nextRoute.id);
+    if (isInWhitelist) return outlet;
+
+    // Has Logged
+    if (usr) return outlet;
+
+    // Not Logged
+    const urlSearchParams = new URLSearchParams();
+    urlSearchParams.set("returnURL", nextRoute.pathname);
+    const query = urlSearchParams.toString();
+    const isGoHome = nextRoute.id === "home";
+    const search = isGoHome ? void 0 : query;
+    const to = { pathname: "/login", search };
+
+    return <Navigate to={to} replace />;
+  }, [matches, searchParams, outlet, usr]);
 
   return (
     <>
-      {routeEl}
+      {routeNode}
       <Backdrop
         open={isLoading}
         sx={{
