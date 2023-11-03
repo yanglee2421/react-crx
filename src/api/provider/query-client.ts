6269b3687950b2bc21543b1ede920ff4121bff89
ZhangSheng <yanglee2421@gmail.com>
2023-11-03 10:27:27 +0800

    query client config

--- src/api/provider/query-client.ts
+++ src/api/provider/query-client.ts
@@ -1,6 +1,10 @@
 // Query Imports
 import { QueryClient, DefaultOptions } from "@tanstack/react-query";
 
+// Persist Imports
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+
 // ** QueryClient
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,11 +25,21 @@ queryClient.setMutationDefaults(["post-demo"], {
   },
 });
 
+// ** Persist
+const persister = createSyncStoragePersister({
+  storage: sessionStorage,
+  key: import.meta.env.VITE_QUERY_PERSISTER_KEY,
+});
+persistQueryClient({ queryClient, persister });
+
 // ** Config
 function queries(): DefaultOptions["queries"] {
   return {
-    staleTime: 1000 * 60 * 5,
-    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60,
+    gcTime: 1000 * 60 * 2,
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
+    refetchOnReconnect: true,
     retryDelay(attemptIndex) {
       return Math.min(1000 * 2 ** attemptIndex, 30000);
     },
