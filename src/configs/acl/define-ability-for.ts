37cf9efd19ead98d5e1ec5139dbf2c63cc19c4af
ZhangSheng <yanglee2421@gmail.com>
2023-11-11 15:58:33 +0800

    sort out code

--- src/configs/acl/define-ability-for.ts
+++ src/configs/acl/define-ability-for.ts
@@ -2,14 +2,13 @@
 import {
   MongoAbility,
   createMongoAbility,
-  CreateAbility,
   AbilityBuilder,
 } from "@casl/ability";
 
-const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
-
 export function defineAbilityFor(role: string) {
-  const { can, build } = new AbilityBuilder(createAppAbility);
+  const { can, cannot, build } = new AbilityBuilder<AppAbility>(
+    createMongoAbility
+  );
 
   switch (role) {
     case "admin":
@@ -18,15 +17,13 @@ export function defineAbilityFor(role: string) {
     case "client":
       can("read", "all");
       break;
+    default:
+      cannot("manage", "all");
   }
 
   return build();
 }
 
-type CRUD = "create" | "read" | "update" | "delete" | "manage";
-type Abilities =
-  | ["read", "User"]
-  | [CRUD, "Article"]
-  | ["manage", "all"]
-  | ["read", "all"];
-export type AppAbility = MongoAbility<Abilities>;
+export type AppAbility = MongoAbility<[Actions, Subjects]>;
+type Actions = "create" | "read" | "update" | "delete" | "manage";
+type Subjects = "all" | "User" | "Article";
