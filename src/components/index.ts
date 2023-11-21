7e9939c6746746d17dbab50d701a733b4d474f24
yanglee2421 <yanglee2421@gmail.com>
2023-11-22 00:15:01 +0800

    Sort out code

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,3 @@ export * from "./input-text";
 export * from "./input-pwd";
 export * from "./input-radio";
 export * from "./input-checkbox";
-
-// ** Login
-export * from "./logout-btn";
--- src/router/router-guard.tsx
+++ src/router/router-guard.tsx
@@ -1,12 +1,11 @@
 // MUI Imports
-import { Backdrop, CircularProgress } from "@mui/material";
+// import { Backdrop, CircularProgress } from "@mui/material";
 
 // Redux Imports
 import {
   mutateSettings,
-  mutateLogin,
   useAppDispatch,
-  useAppSelector,
+  // useAppSelector
 } from "@/redux";
 
 // React Imports
@@ -14,74 +13,28 @@ import React from "react";
 
 // Router Imports
 import {
-  useMatches,
-  useSearchParams,
-  Navigate,
+  // useMatches,
+  // useSearchParams,
+  // Navigate,
   useOutlet,
 } from "react-router-dom";
-import { toIsInWl } from "./router-whitelist";
+// import { toIsInWl } from "./router-whitelist";
 
 export function Component() {
   const dispatch = useAppDispatch();
-  const isLoading = useAppSelector((s) => s.login.isLoading);
-  const usr = useAppSelector((s) => s.login.usr);
 
   React.useEffect(() => {
     dispatch(mutateSettings({}));
-    dispatch(mutateLogin({}));
   }, [dispatch]);
 
   // Router Hooks
   const outlet = useOutlet();
-  const matches = useMatches();
-  const [searchParams] = useSearchParams();
+  // const matches = useMatches();
+  // const [searchParams] = useSearchParams();
 
   const routeNode = React.useMemo(() => {
-    const nextRoute = matches[matches.length - 1];
-    if (!nextRoute) {
-      console.error("Invalid route");
-      return null;
-    }
+    return outlet;
+  }, [outlet]);
 
-    // To Login
-    const isToLogin = nextRoute.id === "login";
-    if (isToLogin) {
-      const returnURL = searchParams.get("returnURL") || "/";
-      return usr ? <Navigate to={returnURL} replace /> : outlet;
-    }
-
-    // To Whitelist
-    const isInWhitelist = toIsInWl(nextRoute.id);
-    if (isInWhitelist) return outlet;
-
-    // Has Logged
-    if (usr) return outlet;
-
-    // Not Logged
-    const urlSearchParams = new URLSearchParams();
-    urlSearchParams.set("returnURL", nextRoute.pathname);
-    const query = urlSearchParams.toString();
-    const isGoHome = nextRoute.id === "home";
-    const search = isGoHome ? void 0 : query;
-    const to = { pathname: "/login", search };
-
-    return <Navigate to={to} replace />;
-  }, [matches, searchParams, outlet, usr]);
-
-  return (
-    <>
-      {routeNode}
-      <Backdrop
-        open={isLoading}
-        sx={{
-          color: "common.white",
-          zIndex(theme) {
-            return theme.zIndex.mobileStepper - 1;
-          },
-        }}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
-    </>
-  );
+  return <>{routeNode}</>;
 }
